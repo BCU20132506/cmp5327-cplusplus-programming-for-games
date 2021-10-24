bba1df0b1afa2615b988dfa7249d60b725ebd98b
BCU20132506 <71894736+BCU20132506@users.noreply.github.com>
2021-10-24 18:55:16 +0100

    Delete code directory

--- code/cplusplus_programming_for_games/source/cplusplus_programming_for_games.cpp
+++
@@ -1,9 +0,0 @@
-// cplusplus_programming_for_games.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Game Over My Friend!" << std::endl;
-    return 0;
-}
